63da8d00607f522eeb48cfa2fd84c4f354d1244e
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-10-19 10:46:05 +0300

    Book showing function changed and now working by modal form input

--- script.js
+++ script.js
@@ -18,57 +18,104 @@ function addBookToLibrary(title, author, pages, read) {
   return myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary("the mobit", "mr.joske", 333, true);
-addBookToLibrary("the susoek", "mr.jorl", 323, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("the susoek3", "mr.jorl", 323, false);
-addBookToLibrary("The Hobbit2", "J.R.R. Tolkien", 295, false);
+// addBookToLibrary("the mobit", "mr.joske", 333, true);
+// addBookToLibrary("the susoek", "mr.jorl", 323, false);
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+// addBookToLibrary("the susoek3", "mr.jorl", 323, false);
+// addBookToLibrary("The Hobbit2", "J.R.R. Tolkien", 295, false);
 
-function showBooksInLibrary() {
-  for (bookNumber in myLibrary) {
-    let book = document.createElement("div");
-    book.classList.add("book");
-    let book_image = document.createElement("div");
-    book_image.classList.add("book_image");
-    let book_image_file = document.createElement("img");
-    book_image_file.classList.add("book_image_file");
-    book_image_file.src = "icons/book.svg";
-    let book_details = document.createElement("div");
-    book_details.classList.add("book_details");
-    let book_title = document.createElement("div");
-    book_title.classList.add("book_title");
-    let book_author = document.createElement("div");
-    book_author.classList.add("book_author");
-    let book_page_status = document.createElement("div");
-    book_page_status.classList.add("book_page_status");
-    let book_page = document.createElement("div");
-    book_page.classList.add("book_page");
-    let book_status = document.createElement("div");
-    book_status.classList.add("book_status");
+// function showBooksInLibrary() {
+//   for (bookNumber in myLibrary) {
+//     let book = document.createElement("div");
+//     book.classList.add("book");
+//     let book_image = document.createElement("div");
+//     book_image.classList.add("book_image");
+//     let book_image_file = document.createElement("img");
+//     book_image_file.classList.add("book_image_file");
+//     book_image_file.src = "icons/book.svg";
+//     let book_details = document.createElement("div");
+//     book_details.classList.add("book_details");
+//     let book_title = document.createElement("div");
+//     book_title.classList.add("book_title");
+//     let book_author = document.createElement("div");
+//     book_author.classList.add("book_author");
+//     let book_page_status = document.createElement("div");
+//     book_page_status.classList.add("book_page_status");
+//     let book_page = document.createElement("div");
+//     book_page.classList.add("book_page");
+//     let book_status = document.createElement("div");
+//     book_status.classList.add("book_status");
 
-    book_title.textContent = myLibrary[bookNumber].title;
-    book_author.textContent = myLibrary[bookNumber].author;
-    book_page.textContent = `${myLibrary[bookNumber].pages} pages`;
-    book_status.textContent = myLibrary[bookNumber].read
-      ? "already read"
-      : "not read yet";
+//     book_title.textContent = myLibrary[bookNumber].title;
+//     book_author.textContent = myLibrary[bookNumber].author;
+//     book_page.textContent = `${myLibrary[bookNumber].pages} pages`;
+//     book_status.textContent = myLibrary[bookNumber].read
+//       ? "already read"
+//       : "not read yet";
 
-    book_details.appendChild(book_title);
-    book_details.appendChild(book_author);
-    book_page_status.appendChild(book_page);
-    book_page_status.appendChild(book_status);
-    book_details.appendChild(book_page_status);
-    book_image.appendChild(book_image_file);
+//     book_details.appendChild(book_title);
+//     book_details.appendChild(book_author);
+//     book_page_status.appendChild(book_page);
+//     book_page_status.appendChild(book_status);
+//     book_details.appendChild(book_page_status);
+//     book_image.appendChild(book_image_file);
 
-    book.appendChild(book_image);
-    book.appendChild(book_details);
+//     book.appendChild(book_image);
+//     book.appendChild(book_details);
 
-    main.appendChild(book);
-  }
+//     main.appendChild(book);
+//   }
+// }
+
+function showAddedBook(
+  bookTitleValue,
+  bookAuthorValue,
+  bookPagesValue,
+  bookReadValue
+) {
+  let book = document.createElement("div");
+  book.classList.add("book");
+  let book_image = document.createElement("div");
+  book_image.classList.add("book_image");
+  let book_image_file = document.createElement("img");
+  book_image_file.classList.add("book_image_file");
+  book_image_file.src = "icons/book.svg";
+  let book_details = document.createElement("div");
+  book_details.classList.add("book_details");
+  let book_title = document.createElement("div");
+  book_title.classList.add("book_title");
+  let book_author = document.createElement("div");
+  book_author.classList.add("book_author");
+  let book_page_status = document.createElement("div");
+  book_page_status.classList.add("book_page_status");
+  let book_page = document.createElement("div");
+  book_page.classList.add("book_page");
+  let book_status = document.createElement("div");
+  book_status.classList.add("book_status");
+
+  book_title.textContent = myLibrary[myLibrary.length - 1].title;
+  book_author.textContent = myLibrary[myLibrary.length - 1].author;
+  book_page.textContent = `${myLibrary[myLibrary.length - 1].pages} pages`;
+  book_status.textContent = myLibrary[myLibrary.length - 1].read
+    ? "already read"
+    : "not read yet";
+
+  book_details.appendChild(book_title);
+  book_details.appendChild(book_author);
+  book_page_status.appendChild(book_page);
+  book_page_status.appendChild(book_status);
+  book_details.appendChild(book_page_status);
+  book_image.appendChild(book_image_file);
+
+  book.appendChild(book_image);
+  book.appendChild(book_details);
+
+  main.appendChild(book);
 }
 
-showBooksInLibrary();
+// showBooksInLibrary();
 
+let bookTitleValue, bookAuthorValue, bookPagesValue, bookReadValue;
 const add_book_dialog = document.querySelector(".add_book_dialog");
 const newBook = document.querySelector(".newBook");
 
@@ -76,6 +123,48 @@ const bookTitle = document.querySelector("#bookTitle");
 const bookAuthor = document.querySelector("#bookAuthor");
 const bookPages = document.querySelector("#bookPages");
 
+const addBook = document.querySelector(".addBook");
+const cancelBook = document.querySelector(".cancelBook");
+const newBookArray = [];
+
 newBook.addEventListener("click", () => {
   add_book_dialog.showModal();
+});
+
+bookTitle.addEventListener("input", (e) => {
+  bookTitleValue = e.target.value;
+});
+
+bookAuthor.addEventListener("input", (e) => {
+  bookAuthorValue = e.target.value;
+});
+
+bookPages.addEventListener("input", (e) => {
+  bookPagesValue = e.target.value;
+});
+
+addBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  const bookRead = document.querySelector('input[name="readStatus"]:checked');
+  bookReadValue = bookRead.value;
+
+  console.log(bookTitleValue, bookAuthorValue, bookPagesValue, bookReadValue);
+  if (bookTitleValue && bookAuthorValue && bookPagesValue && bookReadValue) {
+    addBookToLibrary(
+      bookTitleValue,
+      bookAuthorValue,
+      bookPagesValue,
+      bookReadValue === "true" ? true : false
+    );
+    showAddedBook(
+      myLibrary[myLibrary.length - 1].title,
+      myLibrary[myLibrary.length - 1].author,
+      myLibrary[myLibrary.length - 1].pages,
+      myLibrary[myLibrary.length - 1].read
+    );
+  }
+});
+
+cancelBook.addEventListener("click", () => {
+  add_book_dialog.close();
 });
